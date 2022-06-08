df9cd5718f1f87e11e3452b5d4d1a15743ed42d6
kenolendzion <kenolendzion@gmail.com>
2022-06-08 22:52:32 +0200

    ClassService Inital.

--- Services/ClassService.py
+++ Services/ClassService.py
@@ -1,30 +1,33 @@
+import sqlite3
+
 class ClassService:
 
-    def AddClass(connection, classObj):
-        """
-        Create a new Class into the class table
-        :param connection:
-        :param classObj:
-        :return: none
-        """
-
-        """
-        try:
-            sql = ''' INSERT INTO class(name,schoolId,teacherId)
-              VALUES(?,?,?,?,?,?) '''
-            cur = connection.cursor()
-            cur.execute(sql, classObj)
-            connection.commit()
-
-        except Error as e:
-            print(e) 
-
-        finally:
+    def AddClass(name, teacherId, schoolId):
+        connection = sqlite3.connect("Database/Register.db")
+        cursor = connection.cursor()
+        cursor.execute("""
+        INSERT INTO Classes 
+            VALUES(?,?,?)
+        """,
+        (name, teacherId, schoolId))
+        connection.commit()
+        connection.close()
         
-        """
-    
-    def GetClass(connection, classId):
-        pass
+    def GetClass(classId):
+        connection = sqlite3.connect("Database/Register.db")
+        cursor = connection.cursor()
+        sqlQuery = "SELECT class.ROWID, class.* FROM CLASSES AS class WHERE class.ROWID = " + str(classId) 
+        cursor.execute(sqlQuery)
+        content = cursor.fetchall()
+        return content
+
+    def GetAllClasses():
+        connection = sqlite3.connect("Database/Register.db")
+        cursor = connection.cursor()
+        sqlQuery = "SELECT class.ROWID, class.* FROM CLASSES AS class"
+        cursor.execute(sqlQuery)
+        content = cursor.fetchall()
+        return content
 
     def GetStudentsOfClass(connection, classId):
         pass
