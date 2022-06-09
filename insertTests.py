5818e89b6ac2d5b376bc4db8a885ee5f2070ec42
KenoLendzion <kenolendzion@gmail.com>
2022-06-09 11:00:05 +0200

    Test hinzugefügt.

--- insertTests.py
+++ insertTests.py
@@ -5,29 +5,45 @@ import Services.SchoolService
 
 from Models import Class
 
-#clservice = Services.ClassService.ClassService
-#stservice = Services.StudentService.StudentService
-#teservice = Services.TeacherService.TeacherService
+clservice = Services.ClassService.ClassService
+stservice = Services.StudentService.StudentService
+teservice = Services.TeacherService.TeacherService
 scservice = Services.SchoolService.SchoolService
+
 # Test StudentService
 #stservice.AddStudent("keno", "lendzion", 69, 1)
 #result = stservice.GetAllStudents()
 #print(result)
 
 # Test ClassService
-#print(clservice.AddClass("newclass", "12313", "1231231"))
+clservice.AddClassWithoutTeacherSchoolIds("KenosKlasse")
+result = clservice.GetAllClasses()
+print(result)
+#clservice.AddClass("newclass", "12313", "1231231")
+
+#stservice.AddStudent("Keno", "Lendzion", 69, 1)
+#stservice.AddStudent("Keno", "Lendzion", 69, 1)
+#stservice.AddStudent("Keno", "Lendzion", 69, 1)
+#stservice.AddStudent("Keno", "Lendzion", 69, 1)
+#stservice.AddStudent("Keno", "Lendzion", 69, 1)
+#stservice.AddStudent("Keno", "Lendzion", 69, 1)
+#stservice.AddStudent("Keno", "Lendzion", 69, 1)
+#stservice.AddStudent("Keno", "Lendzion", 69, 1)
 #singleClass = print(clservice.GetClass(2))
 #print(singleClass)
 #result = clservice.GetAllClasses()
+#result = clservice.GetStudentsOfClass(1)
+#
 #print(result)
-
 
 # Test TeacherService
 #teservice.AddTeacher("keno", "lendzion", 69)
+
+#teservice.AddTe
 #result = teservice.GetAllTeachers()
 #print(result)
 
 # Test TeacherService
-scservice.AddSchool("keno", "lendzion", "69")
-result = scservice.GetAllSchools()
-print(result)
+#scservice.AddSchool("keno", "lendzion", "69")
+#result = scservice.GetAllSchools()
+#print(result)
